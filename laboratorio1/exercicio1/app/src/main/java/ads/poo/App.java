2a7bd2b572a56a51628154c26d1091613d6732a9
DaviDelfino12 <delfinodavi1212@gmail.com>
2024-03-26 22:23:53 -0300

    lab1

---
+++ laboratorio1/exercicio1/app/src/main/java/ads/poo/App.java
@@ -0,0 +1,13 @@
+
+package ads.poo;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        
+
+    }
+}
+
---
+++ laboratorio1/exercicio2/app/src/main/java/ads/poo/Horario.java
@@ -0,0 +1,96 @@
+package ads.poo;
+
+public class Horario {
+
+
+    private int hr;
+    private int min;
+    private int seg;
+    
+    private final String[] hrE = {  "zero", "uma", "duas", "tres", "quatro", "cinco", "seis", "sete", "oito", 
+                                    "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte", 
+                                    "vine e um", "vinte e dois", "vinte e três", "vinte e quatro" };
+    
+    private final String[] minesegE = { "zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", 
+                                    "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove",};
+
+    private final String[] dezennasE = { "vinte", "trinta", "quarenta", "cinquenta"};
+ 
+    public Horario() {
+        this.hr = 0;
+        this.min = 0;
+        this.seg = 0;
+    }
+
+    public Horario(int hr) {
+        if(hr >= 0 && hr < 24) {
+            this.hr = hr;
+        } else {
+            this.hr = 0;
+            this.min = 0;
+            this.seg = 0;
+        }
+    }
+
+    public Horario(int hr, int min) {
+        if(hr >= 0 && hr < 24 && min >= 0 && min < 60) {
+            this.hr = hr;
+            this.min = min;
+        } else {
+            this.hr = 0;
+            this.min = 0;
+            this.seg = 0;
+        }
+
+    }
+
+    public Horario(int hr, int min, int seg) {
+        if(hr >= 0 && hr < 24 && min >= 0 && min < 60 && seg >= 0 && seg < 60) {
+            this.hr = hr;
+            this.min = min;
+            this.seg = seg;
+        } else {
+            this.hr = 0;
+            this.min = 0;
+            this.seg = 0;
+        }
+    }
+
+    public boolean setHr(int hr) {
+        if(hr >= 0 && hr < 24) {
+        this.hr = hr;
+        return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean setMin(int min) {
+        if(min >= 0 && min < 60) {
+        this.min = min;
+        return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean setSeg(int seg) {
+        if(seg >= 0 && seg < 60) {
+        this.seg = seg;
+        return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return hr + ":" + min + ":" + seg;
+    }
+
+ 
+
+
+}
+
+
