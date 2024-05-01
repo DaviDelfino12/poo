dea5c6c52cb84cde3a712d75d297d30493125ef7
delfinodavi1212@gmail.com <delfinodavi1212@gmail.com>
2024-04-30 22:22:49 -0300

    aula16

---
+++ aula-15/app/src/main/java/ads/poo/Contato.java
@@ -0,0 +1,13 @@
+package ads.poo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Contato {
+
+    private String nome;
+    private String sobrenome;
+    private LocalDate dataNacs;
+    private ArrayList<Telefone> telefones;
+    private ArrayList<Email> emails;
+}
